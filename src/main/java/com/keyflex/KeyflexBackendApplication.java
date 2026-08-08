ff951a1187ebc7683610a63d90bbcb81bfd7f5a6
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-08 16:37:33 +0300

    initial commit

---
+++ src/main/java/com/keyflex/KeyflexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.keyflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyflexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyflexBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keyflex/KeyflexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyflexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
