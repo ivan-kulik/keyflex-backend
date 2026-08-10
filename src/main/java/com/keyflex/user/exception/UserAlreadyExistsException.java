691ee91e1a6af78e045d5ccc2b9bbcb26365491b
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-10 19:50:59 +0300

    feat(exception): create UserAlreadyExistsException

---
+++ src/main/java/com/keyflex/user/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.keyflex.user.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
