06035ae2fed2c683f9091f9851d114dc3cb1a970
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-11 18:54:19 +0300

    feat(email): enable async and scheduling in KeyflexBackendApplication

--- src/main/java/com/keyflex/KeyflexBackendApplication.java
+++ src/main/java/com/keyflex/KeyflexBackendApplication.java
@@ -2,8 +2,12 @@ package com.keyflex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class KeyflexBackendApplication {
 
     public static void main(String[] args) {
